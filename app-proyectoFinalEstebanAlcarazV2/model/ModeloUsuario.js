556f2c7fcbcbbbb08253857430471b891d387315
EstebanAlcaraz <estebanalfa12@hotmail.com>
2024-08-17 19:50:24 -0500

    encarpetado

---
+++ app-proyectoFinalEstebanAlcarazV2/model/ModeloUsuario.js
@@ -0,0 +1,14 @@
+let usuarios = [
+    {
+      usuario: "ESTEBANALCA",
+      contrasena: "202424",
+      correo: "afa@gmail.com",
+    },
+    {
+      usuario: "USUARIO",
+      contrasena: "200020",
+      correo: "usuARIO@gmail.com",
+    },
+  ];
+  
+  export default usuarios;
